83b192a803330568afe499084305d8daf6076b93
Jason Mitchell <mitchej+github@gmail.com>
2026-06-14 19:26:02 -0700

    SortingIndex override config for modpacks (#23)

--- src/main/java/com/mitchej123/jarjar/Config.java
+++ src/main/java/com/mitchej123/jarjar/Config.java
@@ -15,6 +15,9 @@ import java.io.Reader;
 import java.io.Writer;
 import java.util.Properties;
 
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+
 /*
  * Note: Don't use Forge's config as this needs to load early without all of Forge's classes loaded
  */
@@ -23,6 +26,8 @@ public class Config {
     private static final Logger LOGGER = LogManager.getLogger("JarJar");
 
     public static final int maxThreads;
+    public static final boolean enableSortingIndexOverrides;
+    public static final Object2IntMap<String> sortingIndexOverrides;
 
     static {
         Properties config = new Properties();
@@ -37,8 +42,27 @@ public class Config {
         final int threads = Integer.parseInt(config.getProperty("maxThreads", "-1"));
         maxThreads = threads > 0 ? threads : Runtime.getRuntime().availableProcessors();
 
+        config.putIfAbsent("enableSortingIndexOverrides", "true");
+        enableSortingIndexOverrides = Boolean.parseBoolean(config.getProperty("enableSortingIndexOverrides"));
+
+        final Object2IntMap<String> overrides = new Object2IntOpenHashMap<>();
+        final String sortingIndex = "SortingIndex.";
+        final int subStrIdx = sortingIndex.length();
+        for (String name : config.stringPropertyNames()) {
+            if (name.startsWith(sortingIndex)) {
+                try {
+                    final String coremodClass = name.substring(subStrIdx);
+                    final int sortOrderOverride = Integer.parseInt(config.getProperty(name).trim());
+                    overrides.put(coremodClass, sortOrderOverride);
+                } catch (NumberFormatException e) {
+                    LOGGER.warn("Ignoring non-integer SortingIndex override {}", name);
+                }
+            }
+        }
+        sortingIndexOverrides = overrides;
+
         try (Writer r = new BufferedWriter(new FileWriter(configLocation))) {
-            config.store(r, "Configuration file for early hodgepodge class transformers");
+            config.store(r, "Coremod SortingIndex override: SortingIndex.<coremod class>=<number> sets a coremod's load order.\nExample: SortingIndex.com.mitchej123.hodgepodge.core.HodgepodgeCore=1");
         } catch (IOException e) {
             LOGGER.error("Error reading configuration file. Will use defaults", e);
         }
